d3dc96f5468d1c5b01ac67dfb1982a359cbdea8c
David Mohr <david.mohr@ardexa.com>
2024-08-29 13:51:47 +1000

    chore: commit artifacts

---
+++ dist/requests/sasl_authenticate_request.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SaslAuthenticateRequest = void 0;
+const sasl_authenticate_response_1 = require("../responses/sasl_authenticate_response");
+const abstract_request_1 = require("./abstract_request");
+class SaslAuthenticateRequest extends abstract_request_1.AbstractRequest {
+    params;
+    responseKey = sasl_authenticate_response_1.SaslAuthenticateResponse.key;
+    static Key = 0x0013;
+    static Version = 1;
+    key = SaslAuthenticateRequest.Key;
+    constructor(params) {
+        super();
+        this.params = params;
+    }
+    writeContent(writer) {
+        writer.writeString(this.params.mechanism);
+        switch (this.params.mechanism) {
+            case "PLAIN":
+                writer.writeUInt32(this.params.password.length + this.params.username.length + 2);
+                writer.writeUInt8(0);
+                writer.writeData(this.params.username);
+                writer.writeUInt8(0);
+                writer.writeData(this.params.password);
+                break;
+            case "EXTERNAL":
+                writer.writeUInt32(0);
+                break;
+            default:
+                throw new Error(`Auth mechanism ${this.params.mechanism} not implemented`);
+        }
+    }
+}
+exports.SaslAuthenticateRequest = SaslAuthenticateRequest;
+//# sourceMappingURL=sasl_authenticate_request.js.map
